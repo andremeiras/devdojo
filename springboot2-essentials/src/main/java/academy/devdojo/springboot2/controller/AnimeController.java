9befed02fe8803b0d1c714c1ac754a3e83f86ef8
Andre Meira <andremeirati@gmail.com>
2021-03-14 23:23:35 -0300

    Utilizando algumas anotações do Lombok, como por exemplo a @Log4j2 para gerar Logs e a @RequiredArgsConstructor para criar o construtor da classe com os argumentos necessários requisitados na requisição.

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -3,27 +3,33 @@ package academy.devdojo.springboot2.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("anime")
 @Log4j2 // Anotation do Lombok para gerar logs 
+@RequiredArgsConstructor // Lombok - faz a criação dos construtores necessários da classe
 public class AnimeController {
 
-    @Autowired
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
 
     @GetMapping(path = "list")
     public List<Anime> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+        return List.of(new Anime("Boku No Hero"), new Anime("Berserk"));
+    }
+    
+    @GetMapping(path = "list2")
+    public List<Anime> list2() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Boku No Hero"), new Anime("Berserk"), new Anime("One Piece"));
     }
 
 }
